b8d2a00c3f7778ac4014c1c0e8eace9bb5ebef08
Alex Oliveira <alexoliveira7x@gmail.com>
2023-03-20 16:05:46 +0000

    start new version

---
+++ packages/flexysearch-react/src/lib/flexysearch-react.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface FlexysearchReactProps {}
+
+export function FlexysearchReact(props: FlexysearchReactProps) {
+  return (
+    <div>
+      <h1>Welcome to FlexysearchReact!</h1>
+    </div>
+  );
+}
+
+export default FlexysearchReact;
